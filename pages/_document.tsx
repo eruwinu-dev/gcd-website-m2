e609fa5567cd6a8196a9cefa6e30e9f5611c26d5
Erwin Joshua Encisa <erwin.joshua.encisa@eee.upd.edu.ph>
2022-10-16 22:11:19 +0800

    Updated link rel tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ const MyDocument = () => {
 		<Html lang="en">
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 				<link
 					as="style"
 					href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600&display=swap"
-					rel="stylesheet preload prefetch"
+					rel="stylesheet"
 					type="text/css"
 					crossOrigin="anonymous"
 				/>
