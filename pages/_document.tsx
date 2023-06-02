1eedce3bab3e66d1fdacf3862519d564d5838e20
eruwinu-dev <erwin.joshua.encisa@eee.upd.edu.ph>
2023-06-02 21:14:01 +0800

    Added Google Tag Manager scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 const MyDocument = () => {
 	return (
@@ -13,8 +14,18 @@ const MyDocument = () => {
 					type="text/css"
 					crossOrigin="anonymous"
 				/>
+				<Script id="googleTagManager" strategy="afterInteractive" dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-NBHD5GN');`}} />
 			</Head>
 			<body>
+				<noscript>
+					<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBHD5GN"
+						height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+				</noscript>
 				<Main />
 				<NextScript />
 			</body>
